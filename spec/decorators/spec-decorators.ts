43d4cf5c45b138155cdaf12df1e025b3d5d5b40c
nirendra shakya <nirendra.shakya@park.edu>
2019-06-02 16:23:47 -0500

    Initial commit

---
+++ spec/decorators/spec-decorators.ts
@@ -0,0 +1,68 @@
+const specDataPropName = 'specData';
+const specMethodPropName = 'specMethod';
+
+export function Spec(name?: string) {
+  return function(constructor: Function) {
+    const specName = name || constructor['name'];
+    console.log('@Spec', specName);
+    describe(specName, () => {
+      let obj = Object.create(constructor.prototype);
+      let props = Object.keys(constructor.prototype);
+      props.forEach(prop => {
+        if (constructor.prototype[specMethodPropName] && constructor.prototype[specMethodPropName].hasOwnProperty(prop)) {
+          constructor.prototype[prop].apply(obj);
+        }
+      });
+    });
+  };
+}
+
+export function SpecMethod(description: string) {
+  return function(target: Object, propertyKey: string, descriptor: PropertyDescriptor) {
+    console.log('@SpecMethod');
+    let original = descriptor.value;
+    descriptor.value = () => {
+      const specData = descriptor[specDataPropName];
+      if (specData) {
+        describe(description || propertyKey, () => {
+          for (let i = specData.length - 1; i >= 0; i--) {
+            const test = <TestDefinition>specData[i];
+            it(test.name, () => {
+              original(...test.data);
+            });
+          }
+        });
+      } else {
+        it(description || propertyKey, () => {
+          original();
+        });
+      }
+    };
+
+    if (!target.hasOwnProperty(specMethodPropName)) Object.defineProperty(target, specMethodPropName, { value: {} });
+
+    target[specMethodPropName][propertyKey] = true;
+    return descriptor;
+  };
+}
+
+export function SpecData(name: string, ...args: any) {
+  return function(target: Object, propertyKey: string, descriptor: PropertyDescriptor) {
+    console.log('@SpecData');
+    if (args && descriptor.value) {
+      if (!descriptor[specDataPropName]) {
+        // debugger;
+        Object.defineProperty(descriptor, specDataPropName, { writable: true, configurable: true });
+        descriptor[specDataPropName] = new Array<TestDefinition>();
+      }
+
+      descriptor[specDataPropName].push({ name: name, data: args });
+    }
+    return descriptor;
+  };
+}
+
+interface TestDefinition {
+  name: string;
+  data: any[];
+}
